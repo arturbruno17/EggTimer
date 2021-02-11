99326219c9ca998a73d8798ef14ffacafb81b2b1
Artur Bruno <arturjuliao20@gmail.com>
2021-02-11 20:38:21 -0300

    feat: Create topic notifications
    Send notifications to a specific group of application users who are subscribed to the "breakfast" topic

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -63,6 +63,7 @@ class EggTimerFragment : Fragment() {
         )
 
         // TODO: Step 3.4 call subscribe topics on start
+        subscribeTopic()
 
         return binding.root
     }
@@ -94,6 +95,21 @@ class EggTimerFragment : Fragment() {
         // TODO: Step 1.6 END create a channel
 
     }
+
+    // TODO: Step 3.3 subscribe to breakfast topic
+    private fun subscribeTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
+    }
+
 
     companion object {
         fun newInstance() = EggTimerFragment()
